465e5935808edaa8dcae2bf198c88269f5ebccf2
murdockcrc <luis@delgado.ms>
2021-10-30 21:04:46 +0200

    first commit

---
+++ bootstrap/README.md
@@ -0,0 +1,8 @@
+# ECMAScript version
+
+Define the version of ECMAScript used for compilation (default is ES3)
+
+```js
+tsc --target
+```
+
---
+++ hello.ts
@@ -0,0 +1,6 @@
+'use strict'
+function greet(person: string, date: Date) {
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("Madison", new Date());
